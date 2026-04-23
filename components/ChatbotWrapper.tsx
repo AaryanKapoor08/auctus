3387bf3cebcc3d98196702bc71d110da39ce1428
Aaryan Kapoor <aaryan.kapoor@unb.ca>
2026-04-23 19:04:35 -0300

    components: add components/ChatbotWrapper.tsx

---
+++ components/ChatbotWrapper.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const AIChatbot = dynamic(() => import("./AIChatbot"), {
+  ssr: false,
+  loading: () => null,
+});
+
+export default function ChatbotWrapper() {
+  return <AIChatbot />;
+}
